d0dd844ba1787964b0e429d3bbc04395919a5674
Rithanya1R <ravirithanya1@gmail.com>
2025-05-28 12:10:56 +0530

    maven-devops program 6 commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,10 @@
+package org.example;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+    @Test
+    public void testMain(){
+        assertEquals("hello", "hello");
+    }
+}
